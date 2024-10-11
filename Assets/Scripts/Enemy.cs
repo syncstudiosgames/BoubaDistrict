6d3eb7a84967c93d38290043923ac47d567f69d0
Ana Ordóñez <146625596+anaxrdonez@users.noreply.github.com>
2024-10-11 18:44:36 +0200

    Notes Sequence Render

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     NoteManager _noteManager;
     List<Note> _deathSequence = new List<Note>();
     int _complexity;
+    
+    // Canvas para mostrsr las notas
+    GameObject sequenceCanvas;
+    List<Image> noteImages = new List<Image>();
 
     public void SetUp(int complexity, NoteManager noteManager)
     {
@@ -17,9 +22,8 @@ public class Enemy : MonoBehaviour
         CreateSequence();
         _noteManager.OnNoteLogged += CheckSequence;
 
-        // Renderizar secuencia:
-        // Coger sprites de las n notas de death sequence.
-        // Crear componentes image con el sprrite y añadirlos al canva.
+        //Renderizar la secuencia
+        CreateSequenceCanvas();
 
         Print();
     }
@@ -45,6 +49,40 @@ public class Enemy : MonoBehaviour
         var notes = _noteManager.Notes;
         return notes[Random.Range(0, notes.Count - 1)];
     }
+    void CreateSequenceCanvas()
+    {
+        sequenceCanvas = new GameObject("NoteSequenceCanvas");
+        sequenceCanvas.transform.SetParent(this.transform); 
+        Canvas canvas = sequenceCanvas.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+
+        RectTransform rectTransform = sequenceCanvas.GetComponent<RectTransform>();
+
+        rectTransform.sizeDelta = new Vector2(3f, 1f); 
+
+        rectTransform.localPosition = new Vector3(0, 1.5f, 0); 
+
+        float totalWidth = _deathSequence.Count * 0.4f; 
+        float startX = -totalWidth / 2; 
+
+        // cada nota en la secuencia crear una imagen la centramos y distribuimos para que mno estén pegadas
+        /////SEGUIR PROBANDO
+        for (int i = 0; i < _deathSequence.Count; i++)
+        {
+            GameObject noteGO = new GameObject($"Note_{i}");
+            noteGO.transform.SetParent(sequenceCanvas.transform);
+
+            Image noteImage = noteGO.AddComponent<Image>();
+            noteImage.sprite = _deathSequence[i].Sprite;
+
+            RectTransform noteRect = noteGO.GetComponent<RectTransform>();
+            noteRect.sizeDelta = new Vector2(0.5f, 0.5f);
+
+            noteRect.anchoredPosition = new Vector2(startX + i * 0.4f, 0); 
+
+            noteImages.Add(noteImage);
+        }
+    }
 
     void CheckSequence()
     {
@@ -65,5 +103,7 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Debug.Log("The enemy died");
+
+        Destroy(sequenceCanvas); //destruir la secuencia
     }
 }
