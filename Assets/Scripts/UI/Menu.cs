a530966b4d454c6982fc4c58b74d59809bb59859
Laurennthx <lauragarciamartin.u@gmail.com>
2024-10-21 12:56:03 +0200

    cambio entre escenas
    Menu funcional

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    
+    public void Jugar()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+
+}
