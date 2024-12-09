3de510741bbf414c47a9c09ddc2841798295c953
iamboredasf <m.raboso.2019@alumnos.urjc.es>
2024-12-09 14:32:59 +0100

    persistent background music

---
+++ Assets/Scripts/UI/MusicManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance = null;
+    public static MusicManager Instance
+    {
+        get { return instance; }
+    }
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnEnable()
+    {
+        // Suscribirse al evento de cambio de escena
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        // Cancelar suscripción al evento de cambio de escena
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // Método llamado cuando se carga una nueva escena
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Verificar si la escena es "GameScene"
+        if (scene.name == "GameScene")
+        {
+            StopMusic();
+        }
+        if (scene.name == "Jazz")
+        {
+            StopMusic();
+        }
+        if (scene.name == "Cyberpunk")
+        {
+            StopMusic();
+        }
+    }
+
+    // Método para detener la música
+    public void StopMusic()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+}
